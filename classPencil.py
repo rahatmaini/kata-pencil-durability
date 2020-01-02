8a82d81641846cb13b88e697e3dfee78eb2f3004
Rahat Maini <rahatmaini@live.com>
2020-01-02 05:41:15 -0500

    Added skeleton functions
    It's getting a bit late, so I have just added comments for minimum requirements for each function for a more attentive Rahat to read tomorow and resume work. Todos and #undecided are plenty...

--- classPencil.py
+++ classPencil.py
@@ -6,4 +6,32 @@ class pencil:
         self.pointDurability = pointDurability
         self.leadLength = leadLength
         self.eraserDurability = eraserDurability
-        #TODO default values
+        #TODO default values?
+
+    def sharpen():
+        #look at length of pencil:
+        #IF length>0 THEN restore original pointDurability AND decrement leadLength
+        #ELSE do nothing, as sharpening no longer restores point durability
+        #TODO need to store original manufacturer pointDurability somewhere, if going to decrement it when writing
+
+    def eraseText(textToErase):
+        #undecided if better to implement as pencil1.eraseText("blah") or to call an erase function, passing the pencil object into it
+
+        #needs to erase last occurence, so backwards string indexing
+        #decrement eraserDurability by 1 with each character erasure
+        #if eraserDurability==0, then stop erasing
+
+        #IDEA turn string into list of characters, iterate through using for/in loop, make decisions accordingly
+
+    def writeText(textToWrite):
+        #same issue as above of indecision
+        
+        #just append parameter (string) to paper.
+
+
+    
+    #TODO editing text function
+
+    #undecided if "paper" will be a data structure or something (pages as objects?) or a simple string will suffice, as the real testing is of the pencil and its simulation, not pages of paper or the like
+
+    
