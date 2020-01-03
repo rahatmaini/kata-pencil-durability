2c6f144e818896580e9701d9fa3a4e2ef919ed0e
Rahat Maini <rahatmaini@live.com>
2020-01-03 02:20:15 -0500

    Sharpen tests written, passed, and sharpen function complete

--- testPencil.py
+++ testPencil.py
@@ -3,11 +3,13 @@ import classPencil #(pointDurability, leadLength, eraserDurability)
 
 class testPencil(unittest.TestCase):
     
-    def test_sharpeningCanBeDone(self): #see if a valid pencil can be sharpened, as intended
+    def test_sharpeningCanBeDone(self): #see if a length>0 pencil can be sharpened (should be)
         ticonderoga = classPencil.pencil(10, 2, 0) #10 pt pencil of length 2 with no eraser
         self.assertTrue(ticonderoga.sharpen())
     
-    
+    def test_sharpeningCantBeDone(self): #see if a pencil that has no length can be sharpened (should not be)
+        blackwing = classPencil.pencil(10, 0, 0) #10 pt pencil of no length and no eraser
+        self.assertFalse(blackwing.sharpen())
 
 
 if __name__ == '__main__':
