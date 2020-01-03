61ccc0aca1e712d826a68dfd7629057766ed08c8
Rahat Maini <rahatmaini@live.com>
2020-01-03 02:42:55 -0500

    Cleaned unit test setup, added one more sharpening test
    A setup function to run before every test, and a new test that sees if the pencil was sharpened to its original durability

--- testPencil.py
+++ testPencil.py
@@ -3,13 +3,26 @@ import classPencil #(pointDurability, leadLength, eraserDurability)
 
 class testPencil(unittest.TestCase):
     
+    def setUp(self):
+        self.ticonderoga = classPencil.pencil(10, 2, 0) #10 pt pencil of length 2 with no eraser
+
     def test_sharpeningCanBeDone(self): #see if a length>0 pencil can be sharpened (should be)
-        ticonderoga = classPencil.pencil(10, 2, 0) #10 pt pencil of length 2 with no eraser
-        self.assertTrue(ticonderoga.sharpen())
+
+        self.assertTrue(self.ticonderoga.sharpen())
     
     def test_sharpeningCantBeDone(self): #see if a pencil that has no length can be sharpened (should not be)
-        blackwing = classPencil.pencil(10, 0, 0) #10 pt pencil of no length and no eraser
-        self.assertFalse(blackwing.sharpen())
+
+        self.ticonderoga.leadLength=0
+        self.assertFalse(self.ticonderoga.sharpen())
+
+    def test_sharpenedToOriginalDurability(self): #see if sharpening pencil returns it to original durability
+        
+        self.ticonderoga.pointDurability-=5 #TODO return to this test once writing has been implemented. 
+        self.ticonderoga.sharpen()
+        self.assertEqual(10,self.ticonderoga.pointDurability) #sharpened ticonderoga should have 10 durability
+
+    #def tearDown(self):
+    #   self.ticonderoga.dispose()
 
 
 if __name__ == '__main__':
